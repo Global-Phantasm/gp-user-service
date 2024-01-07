46a3c5f1a0344cf59ce8a5f80aa1cff27d703d69
Dheeraj Kumar <104120915+Dheeraj-Kumar04@users.noreply.github.com>
2024-01-07 14:52:29 +0530

    Create user Test Fix

--- src/test/java/com/gp/pf/user/service/ApplicationTests.java
+++ src/test/java/com/gp/pf/user/service/ApplicationTests.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -18,13 +18,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gp.pf.user.service.dto.UserRequest;
 import com.gp.pf.user.service.repository.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
-@AutoConfigureMockMvc
 @Testcontainers
+@AutoConfigureMockMvc
+@Slf4j
 class ApplicationTests {
 
 	@Container
-	static MongoDBContainer container = new MongoDBContainer("mongo:7.0.5");
+	static MongoDBContainer container = new MongoDBContainer("mongo:latest");
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -36,6 +39,7 @@ class ApplicationTests {
 	@Autowired
 	UserRepository repository;
 	
+	@DynamicPropertySource
 	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
 		dynamicPropertyRegistry.add("spring.data.mongodb.url", container::getReplicaSetUrl);
 	}
@@ -43,17 +47,19 @@ class ApplicationTests {
 	@Test
 	void shouldCreateUser() throws Exception {
 		UserRequest userRequest = getUserRequest();
+		int initialSize = repository.findAll().size();
+		log.info("Initially there are {} records.",initialSize);
 		String requestString = mapper.writeValueAsString(userRequest);
 		mockMvc.perform(
 				MockMvcRequestBuilders.post("/api/user")
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(requestString)
 		);
-		
-		Assertions.assertEquals(repository.findAll().size(),1);
+		log.info("Create Request Completed");
+		Assertions.assertEquals(repository.findAll().size(),initialSize+1);
+		log.info("Read Request Completed");
 
 	}
-
 
 	private UserRequest getUserRequest() {
 		return UserRequest.builder().username("dheerajkumar04")
